d5b13472a7dc34d59535a7d89de20fa6a80377b2
Joao Pedro Moura <jpmourabraga@gmail.com>
2023-02-05 02:33:50 -0300

    Changed some linting config

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -12,7 +12,10 @@ class ModelTest(TestCase):
         """Test creating a user with an email is successful."""
         email = "test@example.com"
         password = "testpass123"
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+            )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -36,7 +39,10 @@ class ModelTest(TestCase):
 
     def test_create_superuser(self):
         """Test creating a superuser."""
-        user = get_user_model().objects.create_superuser("test@example.com", "test123")
+        user = get_user_model().objects.create_superuser(
+            "test@example.com",
+            "test123"
+            )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
